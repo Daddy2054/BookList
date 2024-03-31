019140a0d2f892967bbac265849b935282fa9b5c
Jean <acalto2@gmail.com>
2024-03-31 09:10:03 -0400

    w3.Importance of data validation

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -2,7 +2,8 @@ from decimal import Decimal
 from rest_framework import serializers
 from .models import MenuItem
 from .models import Category
-
+from rest_framework.validators import UniqueValidator
+from rest_framework.validators import UniqueTogetherValidator
 
 # # to hide some fields from json
 # class MenuItemSerializer(serializers.Serializer):
@@ -23,7 +24,7 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class MenuItemSerializer(serializers.ModelSerializer):
-#class MenuItemSerializer(serializers.HyperlinkedModelSerializer):
+    # class MenuItemSerializer(serializers.HyperlinkedModelSerializer):
     stock = serializers.IntegerField(source="inventory")
     price_after_tax = serializers.SerializerMethodField(method_name="calculate_tax")
     category = CategorySerializer(read_only=True)
@@ -31,6 +32,33 @@ class MenuItemSerializer(serializers.ModelSerializer):
     #     queryset=Category.objects.all(), view_name="category-detail"
     # )
     category_id = serializers.IntegerField(write_only=True)
+    # Method 1: Conditions in the field
+    # price = serializers.DecimalField(max_digits=6, decimal_places=2, min_value=2)
+
+    # Method:3 Using validate_field() method
+    # def validate_price(self, value):
+    #     if (value < 2):
+    #         raise serializers.ValidationError('Price should not be less than 2.0')
+    #     return value
+
+    # def validate_stock(self, value):
+    #     if (value < 0):
+    #         raise serializers.ValidationError('Stock cannot be negative')
+    #     return value
+
+    # Method 4: Using the validate() method
+    # def validate(self, attrs):
+    #     if attrs["price"] < 2:
+    #         raise serializers.ValidationError("Price should not be less than 2.0")
+    #     if attrs["inventory"] < 0:
+    #         raise serializers.ValidationError("Stock cannot be negative")
+    #     return super().validate(attrs)
+
+    # UniqueValidator v1
+    # title = serializers.CharField(
+    #     max_length=255, validators=[UniqueValidator(queryset=MenuItem.objects.all())]
+    # )
+
     class Meta:
         model = MenuItem
         depth = 1
@@ -41,7 +69,24 @@ class MenuItemSerializer(serializers.ModelSerializer):
             "stock",
             "price_after_tax",
             "category",
-            'category_id',
+            "category_id",
+        ]
+        # Method 2: Using keyword arguments in the Meta class
+        # extra_kwargs = {
+        #     "price": {"min_value": 2},
+        #     "inventory": {"min_value": 0},
+        # }
+
+        # UniqueValidator v2
+        # extra_kwargs = {
+        #     "title": {"validators": [UniqueValidator(queryset=MenuItem.objects.all())]}
+        # }
+
+        # UniqueTogetherValidator
+        validators = [
+            UniqueTogetherValidator(
+                queryset=MenuItem.objects.all(), fields=["title", "category_id"]
+            )
         ]
 
     def calculate_tax(self, product: MenuItem):
