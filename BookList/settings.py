d9e2262e56096ad2c979f5df1cef6669ae0dafa2
Jean <acalto2@gmail.com>
2024-03-31 19:21:02 -0400

    w3.Registration and authentication endpoints with JWT

--- BookList/settings.py
+++ BookList/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "LittleLemonAPI",
     "LittleLemonDRF",
     "djoser",
+    "rest_framework_simplejwt",
+    "rest_framework_simplejwt.token_blacklist",
 ]
 
 MIDDLEWARE = [
@@ -152,6 +154,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 3,
     "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ),
--- BookList/urls.py
+++ BookList/urls.py
@@ -1,15 +1,32 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenBlacklistView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     # path('api/', include('BookListDRF.urls')),
-    path('api/', include('LittleLemonAPI.urls')),
+    path("api/", include("LittleLemonAPI.urls")),
     # path('api/', include('BookListAPI.urls')),
     # path('api/' ,include('LittleLemonDRF.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
-
-    #Djoser
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
+    # Djoser
+    path("auth/", include("djoser.urls")),
+    path("auth/", include("djoser.urls.authtoken")),
+    # JWT
+    path(
+        "api/token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair",
+    ),
+    path(
+        "api/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
+    path(
+        "api/token/blacklist/",
+        TokenBlacklistView.as_view(),
+        name="token_blacklist",
+    ),
 ]
